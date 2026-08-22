8a5d7e8c670b86fda9709905b5a1d165243c3fd4
HongSin-J4F <hong.sin966@gmail.com>
2026-08-22 17:17:33 +0700

    feat: log port on startup in telegram-bot-api entrypoint.sh

--- telegram-bot-api/entrypoint.sh
+++ telegram-bot-api/entrypoint.sh
@@ -8,6 +8,8 @@ set -e
 : "${TELEGRAM_API_ID:?TELEGRAM_API_ID must be set (see https://my.telegram.org)}"
 : "${TELEGRAM_API_HASH:?TELEGRAM_API_HASH must be set (see https://my.telegram.org)}"
 
+echo "Starting Telegram Bot API on port ${PORT:-8081}"
+
 exec telegram-bot-api \
     --api-id="${TELEGRAM_API_ID}" \
     --api-hash="${TELEGRAM_API_HASH}" \
