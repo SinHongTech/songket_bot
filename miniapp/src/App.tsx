4f09f8d3e219f4c52d502f6e63a3b73ba54a4a73
SaoSochealika <sochealikasao@gmail.com>
2026-08-31 15:20:37 +0700

    fix logo

--- miniapp/src/App.tsx
+++ miniapp/src/App.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import PublicApp from "./public/PublicApp";
 import AdminApp from "./admin/AdminApp";
-
+import LogoSplash from "./public/components/LogoSplash";
 const router = createBrowserRouter([
   { path: "/", Component: PublicApp },
   { path: "/dashboard", Component: AdminApp },
+  { path: "/splash", Component: LogoSplash },
 ]);
 
 export default function App() {
